eb85e1ab4937ac443143a7c306a3b357bae4ce8a
MercedeAbedi <108922081+MercedeAbedi@users.noreply.github.com>
2022-12-11 21:18:30 +0330

    Add files via upload

---
+++ Guess_words_Game.py
@@ -0,0 +1,40 @@
+import random
+
+def game(words):
+    guesses = ''
+    turns = 12
+    word = words.split()
+    secret_letter = random.choice(word)
+    
+    while turns > 0:
+        failed = 0
+        for char in secret_letter:
+            if char in guesses:
+                print(char, end=" ")
+ 
+            else:
+                print("_")
+                failed += 1
+        
+        if failed == 0:
+            print("You Win")
+            print("The word is: ", secret_letter)
+            break
+    
+        print('\n')
+        guess = input("guess a character:")
+    
+        guesses += guess
+    
+        if guess not in secret_letter:
+            turns -= 1
+            print("Wrong")
+        
+            print("You have", + turns, 'more guesses')
+        
+            if turns == 0:
+                print("You Loose")
+
+my_words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'
+
+game(my_words)
